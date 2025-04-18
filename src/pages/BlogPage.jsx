9eecb27bc9a9a7d12be3f8439b9f0c43736f131c
Fox <y.montenegro@hotmail.com>
2025-04-18 12:55:29 -0500

    Blog con conexion a API, ya trae los datos

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
 import HeaderComponent from "../components/HeaderComponent"
 
 
 function BlogPage() {
+
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    console.log(data);
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [])
+
   return (
     <>
       <HeaderComponent></HeaderComponent>
