441962f38a822b7ff466e2cd06be99e9d6370c5d
Fox <y.montenegro@hotmail.com>
2025-04-18 12:24:30 -0500

    Finalizacioin rutas - paginas, funciona correctamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import HomePage from './Pages/HomePage'
-import BlogPage from './Pages/BlogPage'
+
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import BlogPage from './pages/BlogPage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
 
         <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/" element={<BlogPage />}></Route>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog" element={<BlogPage />} />
         </Routes>
 
     
-  )
+  );
 }
 
 export default App
