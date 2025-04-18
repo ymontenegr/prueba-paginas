dbaf13120e32448d40314d068a76fe6ef8e804b1
Fox <y.montenegro@hotmail.com>
2025-04-18 12:10:32 -0500

    Se incluyeron las paginas y la configuracion, aunque aun no muestra nada

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './Pages/HomePage'
+import BlogPage from './Pages/BlogPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    
-      <div>
-        <h1>Prueba Páginas</h1>
 
-      </div>
-
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/" element={<BlogPage />}></Route>
+        </Routes>
 
     
   )
